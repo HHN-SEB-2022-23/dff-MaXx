90d01a3d73c95f5128ff179124ef2e346a5ee7a0
Jan-Niklas <jan-niklas.jaeger@ejbk.de>
2023-01-02 17:15:19 +0100

    Move Klasse implementiert

--- src/main/java/de/hhn/Move.java
+++ src/main/java/de/hhn/Move.java
@@ -1,4 +1,13 @@
 package de.hhn;
 
+import de.hhn.lib.Vector2D;
+
 public class Move {
+    public final CharacterKind characterKind;
+    public final Vector2D target;
+
+    public Move (CharacterKind characterKind, Vector2D target){
+        this.characterKind = characterKind;
+        this.target = target;
+    }
 }
