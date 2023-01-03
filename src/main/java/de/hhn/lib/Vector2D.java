477719da883082643594fa794af56dbf1c52c7e4
Frank-Mayer <mail@frank-mayer.io>
2023-01-03 16:36:39 +0100

    Vector2D von Felix

--- src/main/java/de/hhn/lib/Vector2D.java
+++ src/main/java/de/hhn/lib/Vector2D.java
@@ -1,4 +1,28 @@
 package de.hhn.lib;
 
 public class Vector2D {
+    int x;
+    int y;
+
+    public Vector2D (int x, int y) {
+        this.y = y;
+        this.x = x;
+
+    }
+
+    public int getX () {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
